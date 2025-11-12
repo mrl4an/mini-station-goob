0d654c45f9d3015b4d89664c0bd7f1438bc960e0
Egorick1845 <proegorweb@gmail.com>
2025-11-12 19:16:20 +0300

    днк локер

---
+++ Content.Server/_Mini/HardsuitIdentification/DNALockerComponent.cs
@@ -0,0 +1,45 @@
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Audio.Systems;
+
+namespace Content.Server._Mini.DNALocker;
+
+[RegisterComponent]
+public sealed partial class DNALockerComponent : Component
+{
+    [DataField]
+    public EntProtoId Action = "ActionHardsuitSaveDNA";
+
+    [DataField]
+    public EntityUid? ActionEntity;
+
+    [DataField]
+    public string DNA = String.Empty;
+
+    [DataField]
+    public bool DNAWasStored = false;
+
+    [DataField]
+    public bool Activated = false;
+
+    /// <summary>
+    /// Emag sound effects.
+    /// </summary>
+    [DataField]
+    public SoundSpecifier SparkSound = new SoundCollectionSpecifier("sparks")
+    {
+        Params = AudioParams.Default.WithVolume(8),
+    };
+
+    [DataField]
+    public bool Nonlethal;
+
+    [DataField("lockSound")]
+    public SoundSpecifier LockSound = new SoundPathSpecifier("/Audio/_Mini/Misc/dna-lock.ogg");
+
+    [DataField("lockerExplodeSound")]
+    public SoundSpecifier LockerExplodeSound = new SoundPathSpecifier("/Audio/Effects/Grenades/SelfDestruct/SDS_Charge.ogg");
+
+    [DataField("deniedSound")]
+    public SoundSpecifier DeniedSound = new SoundPathSpecifier("/Audio/Effects/Cargo/buzz_sigh.ogg");
+}
